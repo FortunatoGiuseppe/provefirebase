3d2739a65f8cc4f3f26d696c6abf3fd4b3c56086
g.diasparra1 <g.diasparra1@studenti.uniba.it>
2022-02-21 16:50:47 +0100

    PROVA stampa piatti

--- app/src/main/java/com/example/casodistudiomamange/fragment/CategoryFragment.java
+++ app/src/main/java/com/example/casodistudiomamange/fragment/CategoryFragment.java
@@ -21,12 +21,15 @@ import com.example.casodistudiomamange.adapter.Adapter_plates;
 import com.example.casodistudiomamange.model.Category;
 import com.example.casodistudiomamange.model.Databasee;
 import com.example.casodistudiomamange.model.Plate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -101,7 +104,21 @@ public class CategoryFragment extends Fragment {
                         }
                     }
                 });*/
-    }
 
+        //problemi: leggere senza sapere codici e trasformare cose lette in oggetto di plate
+        DocumentReference docRef = db.collection("Categorie").document(CategoryKey);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                    }
+                }
+            }
+
+        });
+    }
 
 }
